057856571844b5bf62cc07859bc8029ba663b3fa
dbogdano <dbogdanoff@hotmail.com>
2022-08-29 20:37:07 -0700

    cleaned up SLR pipeline

--- SLR_pipeline.sh
+++ SLR_pipeline.sh
@@ -31,11 +31,6 @@ fiveprime_R2=$input_dir_5prime/*R3_001.fastq.gz
 fiveprime_I1=$input_dir_5prime/*R2_001.fastq.gz
 
 
-### first add the TELLSEQ barcodes to read names using sinto
-
-#sinto barcode --bases 18 --barcode_fastq $threeprime_I1 --read1 $threeprime_R1 --read2 $threeprime_R2
-#sinto barcode --bases 18 --barcode_fastq $internal_I1 --read1 $internal_R1 --read2 $internal_R2
-#sinto barcode --bases 18 --barcode_fastq $fiveprime_I1 --read1 $fiveprime_R1 --read2 $fiveprime_R2
 
 #export compress_fastq_threads="8"
 : '
@@ -165,38 +160,6 @@ STAR \
 '
 
 
-#Add read group tags designating library type
-
-: '
-picard AddOrReplaceReadGroups \
-	I=$outdir/3prime_alignment/Aligned.sortedByCoord.out.bam \
-	O=$outdir/3prime_alignment/Aligned.sortedByCoord.tag.bam \
-	ID=3prime \
-	LB=3prime \
-	PL=ILLUMINA \
-	PU=SLR \
-	SM=GW21_1
-
-picard AddOrReplaceReadGroups \
-        I=$outdir/internal_alignment/Aligned.sortedByCoord.out.bam \
-	O=$outdir/internal_alignment/Aligned.sortedByCoord.tag.bam \
-	ID=internal \
-	LB=internal \
-	PL=ILLUMINA \
-	PU=SLR \
-	SM=GW21_1
-
-picard AddOrReplaceReadGroups \
-	I=$outdir/5prime_alignment/Aligned.sortedByCoord.out.bam \
-	O=$outdir/5prime_alignment/Aligned.sortedByCoord.tag.bam \
-	ID=5prime \
-        LB=5prime \
-        PL=ILLUMINA \
-        PU=SLR \
-        SM=GW21_1	 
-'
-
-
 ##merge BAM files and attach RG tag
 
 mkdir -p $outdir/merged/
@@ -209,18 +172,9 @@ samtools merge $outdir/merged/merged.bam \
 	$outdir/3prime_alignment/Aligned.sortedByCoord.out.bam \
 	$outdir/internal_alignment/Aligned.sortedByCoord.out.bam \
 	$outdir/5prime_alignment/Aligned.sortedByCoord.out.bam
-
-
-
-picard MergeSamFiles \
-      I=$outdir/3prime_alignment/Aligned.sortedByCoord.tag.bam \
-      I=$outdir/internal_alignment/Aligned.sortedByCoord.tag.bam \
-      I=$outdir/5prime_alignment/Aligned.sortedByCoord.tag.bam \
-      O=$outdir/merged/merged.bam \
-      USE_THREADING=true \
-      CREATE_INDEX=true
-
 '
+
+
 
 ##add missing Gene alignents 
 TMPDIR=$outdir/merged/_tmp
@@ -229,21 +183,13 @@ mkdir -p $TMPDIR
 
 export TMPDIR
 
-python /media/chang/HDD-10/derek/SLR/scripts/add_gene_tag.py \
+python /home/derek/analysis_10/SLR/scSLR/scripts/add_gene_tag_parallel.py \
 	$GTF_FILE \
 	$outdir/merged/merged.bam \
 	12
 
 
 : '
-##remove reads without gene tag
-
-picard FilterSamReads \
-	I=$outdir/merged/merged.tagged.bam \
-	O=$outdir/merged/merged_tag.bam \
-	TAG=GN \
-	TAG_VALUE=- \
-	Filter=excludeTagValues
 
 
 ##Add TellSeq barcodes as tags, from read names
@@ -287,96 +233,4 @@ python share_UMIs.py \
 echo "SLR pipeline completed"
 
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-#TELL_BARCODE_WHITELIST=$BC_whitelist
- 
-##create intersection of TellSeq barcode whitelists
-#awk 'NR==FNR { lines[$0]=1; next } $0 in lines' $TellREAD_3prime_whitelist $TellREAD_internal_whitelist > TellREAD_intersection_whitelist.txt
-# 
-#LINKED_BARCODE_WHITELIST=TellREAD_intersection_whitelist.txt
-# 
-# 
-# 
-#Python /media/chang/HDD-4/derek/SLR/slr-paper/merge.py \
-#      --tsw ./TellREAD_intersection_whitelist.txt \
-#      --cbw $BC_whitelist \
-#      --internal-bam $BAM1 \
-#      --three-prime-umi $BAM2 \
-#      --three-prime-ts $BAM3 \
-#      --three-prime-out 3prime_out.bam \
-#      --internal-out internal_out.bam
-#  
-#samtools merge merged.bam 3prime_out.bam internal_out.bam
- 
-#samtools sort -@ 16 merged.bam -o merged_sorted.bam
- 
-#samtools index -@ 16 merged_sorted.bam
-# 
-# 
-###run stats.py
-#python stats.py
-#
-#
- 
-###generate fastqs from alignments for input into kallisto
-##
-##icard SamToFastqWithTags \
-##      	I=$INTERNAL-OUT \
-##dsd    	FASTQ=R1.fq \
-##      	SECOND_END_FASTQ=R2.fq \
-#      	SEQUENCE_TAG_GROUP="CB,UB" \
-#      	QUALITY_TAG_GROUP="CY,UY"
-
-
-
-
-
-#picard MarkDuplicates \
- #       I=$INPUT-BAM \
-  #      O=$MARKED-BAM \
-   #     M=marked_metrics.txt \
-    #    BARCODE_TAG=CR
-#
-#
-#
-#
-#
-##index bam files
-#samtools index -@ 16 internal_TELLSEQ/Aligned.sortedByCoord.out.bam
-#samtools index -@ 16 3prime_10X_Aligned/sortedByCoord.out.bam
-#samtools index -@ 16 3prime_TELLSEQ_Aligned/sortedByCoord.out.bam
-#
-#
-##mark duplicates with picard
-#picard MarkDuplicates \
-#       I=internal_TELLSEQ/Aligned.sortedByCoord.out.bam \
-#       O=internal_TELLSEQ/Aligned.MarkDuplicates.out.bam \
-#       M=internal_MarkDuplicates_metrics.txt \
-#      BARCODE_TAG=CB
-#
-#
 
